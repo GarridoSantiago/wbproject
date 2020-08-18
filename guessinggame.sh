f5c67fd11da58a70f6545b44089efcd3c41ea655
GarridoSantiago <garridosantiagob@gmail.com>
2020-08-18 13:11:26 -0500

    try until you guess

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,22 @@
 function tolowmuch {
 	if [[ $1 -lt $(ls | wc -l) ]] && [[ $1 =~ [0-9]+ ]]
 	then
-		echo "Your guess is to low"
+		echo "==Your guess is to low. Try again=="
 	elif [[ $1 -gt $(ls | wc -l) ]] && [[ $1 =~ [0-9]+ ]]
 	then
-		echo "Your guess is to high"
+		echo "==Your guess is to high Try again=="
 	elif [[ $1 -eq $(ls | wc -l) ]] && [[ $1 =~ [0-9]+ ]]
 	then
-		echo "CONGRATULATIONS! YOUR GUESS IS RIGHT!"
+		echo "==CONGRATULATIONS! YOUR GUESS IS RIGHT!=="
+		f=1
 	else
-		echo "That was wrong :("
+		echo "==That was wrong :(. Try again=="
 	fi
 }
-
-read guess
-tolowmuch $guess
+f=0
+echo "Take a guess. How many files you think are in this directory?  "
+while [[ $f -eq 0 ]]
+do
+	read guess
+	tolowmuch $guess
+done
